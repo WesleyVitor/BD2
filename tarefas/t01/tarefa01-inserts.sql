a0785956b7ddab1aa8156f587e8a8eca2695070c
WesleyVitor <wesleydemorais@outlook.com.br>
2022-04-29 20:02:03 -0300

    feat: Inserindo projetos #1

--- tarefas/t01/tarefa01-inserts.sql
+++ tarefas/t01/tarefa01-inserts.sql
@@ -1,3 +1,4 @@
+/* Inserindo Departamentos */
 insert into departamento
 (descricao, cod_gerente)
 values ( 'Dep História', null);
@@ -17,7 +18,7 @@ values (null, null);
 insert into departamento
 ( descricao, cod_gerente)
 values ('Dep Direito', null);
-
+/* Inserindo Funcionários */
 insert into funcionario
 (nome, sexo, dtNasc, salario, codDepto)
 values ('Ana', 'F', '1988-05-07', 2500.00, 1);
@@ -46,3 +47,19 @@ update departamento set cod_gerente = 19 where codigo = 2;
 update departamento set cod_gerente = 20 where codigo = 3;
 update departamento set cod_gerente = 22 where codigo = 4;
 update departamento set cod_gerente = 23 where codigo = 5;
+
+/* Inserindo Projetos */
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+values ('APF', 'Analisador de Ponto de Função', 2, 19, '2018-02-26', '2019-06-30');
+
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+values ('Monitoria', 'Projeto de Monitoria 2019.1', 4, 21, '2019-02-26', '2019-12-30');
+
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+values ('BD', 'Projeto de Banco de Dados', 3, 22, '2018-02-26', '2018-06-30');
+
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+values ('ES', 'Projeto de Engenharia de Software', 1, 19, '2018-02-26', '2018-06-30');
+
+insert into projeto(nome, descricao, cod_depto, cod_responsavel, data_inicio, data_fim)
+values ('DL', 'Projeto de Direito e Legislação', 1, 19, '2019-02-26', '2019-06-30');
