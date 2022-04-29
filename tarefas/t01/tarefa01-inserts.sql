0d9e1e2a8e32ff965c334a2636578c2b7e18b55c
WesleyVitor <wesleydemorais@outlook.com.br>
2022-04-29 19:45:57 -0300

    feat: Inserindo departamentos #1

---
+++ tarefas/t01/tarefa01-inserts.sql
@@ -0,0 +1,19 @@
+insert into departamento
+(descricao, cod_gerente)
+values ( 'Dep História', null);
+
+insert into departamento
+(descricao, cod_gerente)
+values ( 'Dep Computação', null);
+
+insert into departamento
+( descricao, cod_gerente)
+values ( 'Dep Geografia', null);
+
+insert into departamento
+( descricao, cod_gerente)
+values (null, null);
+
+insert into departamento
+( descricao, cod_gerente)
+values ('Dep Direito', null);
