62f2adf6cc7036d4eec7de927ad0d92332cc44d2
WesleyVitor <wesleydemorais@outlook.com.br>
2022-06-26 13:14:02 -0300

    feat: Inserir Funcionário #3

---
+++ tarefas/t03/odbc/dml/inserirFuncionario.go
@@ -0,0 +1,26 @@
+package dml
+
+import (
+	"fmt"
+
+	"github.com/WesleyVitor/dcl"
+)
+
+
+func InserirFuncionario(){
+	db := dcl.Connect()
+
+	insert := fmt.Sprint(`
+		INSERT INTO funcionario(
+			nome, sexo, dtNasc, salario, codSupervisor, codDepto)
+		values ('Maria', 'F', '1981-07-01', 2500.00, null, null); 	
+		
+		INSERT INTO funcionario(
+			nome, sexo, dtNasc, salario, codSupervisor, codDepto)
+		values ('José', 'M', '1981-07-01', 1500.00, null, null); 	
+		
+	`)
+	_,err := db.Exec(insert)
+	dcl.HandlerError(err)
+
+}
